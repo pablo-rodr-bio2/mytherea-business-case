24a1b704b4e515cc09469bc8c3db8e9fcfdf0289
pablo-rodr-bio2 <pablo.rodriguez@toptal.com>
2024-11-17 14:17:48 +0100

    Adding link to carousel

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import './Carousel.scss'
+import { Link } from 'react-router-dom'
 
 const Carousel = ({ movies, loading, error }) => {
   if (loading) {
@@ -13,11 +14,20 @@ const Carousel = ({ movies, loading, error }) => {
   
   return (
     <section className="carousel">
-      <div>Previous</div>
-      {movies && movies.slice(0,3).map(movie => (
-        <img src={movie.Poster} alt={movie.Title} /> 
-      ))}
-      <div>Next</div>
+      <div className='carousel-button'>Previous</div>
+
+      <div className="movies">
+        {movies && movies.slice(0,3).map(movie => (
+          <div className='movie'>
+            <Link to={`/movies/${movie.imdbID}`}>
+              {/* <img className='carousel-img' src={movie.Poster} alt={movie.Title} />  */}
+              <p>{movie.Title}</p>
+            </Link>
+          </div>
+        ))}
+      </div>
+
+      <div className='carousel-button'>Next</div>
     </section>
   )
 }
